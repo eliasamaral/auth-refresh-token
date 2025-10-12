9efd22392023eee60f8fb530f94a6b263dfbff96
Elias Amaral <eliasseverianoamaral@gmail.com>
2025-10-11 23:50:26 -0300

    Atualização da configuração do docker-compose, refinamento dos scripts package.json e remoção do JWT codificado no teste de força bruta. Volume adicionado para persistência de dados do PostgreSQL.

--- src/utils/brute.force.test.js
+++ src/utils/brute.force.test.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoidG9rZW4iLCJpYXQiOjE3NjAwNTcyNDQsImV4cCI6MTc2MDA1OTA0NCwic3ViIjoiYWZlZjJmN2QtYTBiNS00ODQ2LTkzOGQtZmM4ZjgyZGM5NzRmIn0.nwwifaD_z4JzQmqcapId6edSnVAaK7guJ-475vBrurY"; // seu JWT
+const token = ""; // JWT
 
 const test = async () => {
 	for (let i = 1; i <= 6; i++) {
